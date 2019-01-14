a4b629d6d1ec48aa3b6cda8e5c3207c7de0dd3a2
zhangyangbin <zhangyangbin@zhangyangbindeMac-mini.local>
2019-01-14 17:40:41 +0800

    use @vue/cli

---
+++ src/main/main.js
@@ -0,0 +1,23 @@
+const { app,BrowserWindow } = require('electron')
+let mainwindow = null
+function createWin(){
+    mainwindow = new BrowserWindow({
+        minHeight: 524,
+        minWidth:856,
+        useContentSize:true,
+        show:true,
+        frame:false,
+        autoHideMenuBar:true,
+        webSecurity:false,
+        titleBarStyle:'hidden',
+    })
+    let localurl = 'http://localhost:8083/index.html'
+    mainwindow.loadURL(localurl)
+    mainwindow.webContents.openDevTools()
+    mainwindow.on('ready-to-show',()=>{
+        mainwindow.show()
+    })
+}
+app.on('ready',()=>{
+    createWin()
+})
